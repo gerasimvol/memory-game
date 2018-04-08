0ad9fd4931303b52ba7d137ee9e9997e4d5ff01b
gerasimvol <volodik1802@gmail.com>
2018-04-08 09:48:04 +0300

    Init project

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <card />
+  </div>
+</template>
+
+<script>
+import Card from './components/Card'
+
+export default {
+  name: 'App',
+  components: {
+    Card
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Card.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="card">
+    <div class="card__title">Card</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Card',
+  data () {
+    return {}
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+ .card {
+   &__title {
+     color: red;
+   }
+ }
+</style>
